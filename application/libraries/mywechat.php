af38f8475db96b36406a1c8dbcd91d9653155d97
kym0913 <km13327911384@gmail.com>
2016-03-14 15:51:10 +0800

    complete version weather

--- application/libraries/mywechat.php
+++ application/libraries/mywechat.php
@@ -34,17 +34,45 @@ class MyWechat extends WechatEx {
      $getweatherinfo=$getreturn['result']['data']['realtime']['weather']['info'];
      $gettemperatureday=$getreturn['result']['data']['weather'][0]['info']['day']['2'];
      $gettemperaturenight=$getreturn['result']['data']['weather'][0]['info']['night']['2'];
+     $getwinddir=$getreturn['result']['data']['weather'][0]['info']['day']['3'];
+     $getwindpowlev=$getreturn['result']['data']['weather'][0]['info']['day']['4'];
 
      $getweatherkindlytipstatus=$getreturn['result']['data']['life']['info']['chuanyi'][0];
      $getweatherkindlytipmore=$getreturn['result']['data']['life']['info']['chuanyi'][1];
 
      $getweatheruptime=date("Y-m-d H", $getweatherchuouptime);
 
+
+     $tom_weainfo=$getreturn['result']['data']['weather'][1]['info']['day']['1'];
+     $tom_gettemperatureday=$getreturn['result']['data']['weather'][1]['info']['day']['2'];
+     $tom_gettemperaturenight=$getreturn['result']['data']['weather'][1]['info']['night']['2'];
+     $tom_getwinddir=$getreturn['result']['data']['weather'][1]['info']['day']['3'];
+     $tome_getwindpowlev=$getreturn['result']['data']['weather'][1]['info']['day']['4'];
+
+     $at_weainfo=$getreturn['result']['data']['weather'][2]['info']['day']['1'];
+     $at_gettemperatureday=$getreturn['result']['data']['weather'][2]['info']['day']['2'];
+     $at_gettemperaturenight=$getreturn['result']['data']['weather'][2]['info']['night']['2'];
+     $at_getwinddir=$getreturn['result']['data']['weather'][2]['info']['day']['3'];
+     $at_getwindpowlev=$getreturn['result']['data']['weather'][2]['info']['day']['4'];
+
+
+
+
+
+
+
      if(!empty($getreturn['result']))
      {
        $wearesult="【".$getweathercity."天气预报】\n".$getweatheruptime."时发布"
-       ."\n\n实时天气\n".$getweatherinfo." ".$gettemperatureday.wechat::DEGREENS_CELSIUS." ~ ".$gettemperaturenight.wechat::DEGREENS_CELSIUS
-       ."\n\n温馨提示："."天气".$getweatherkindlytipstatus."\n".$getweatherkindlytipmore;
+       ."\n\n实时天气\n".$getweatherinfo." ".$gettemperatureday.wechat::DEGREENS_CELSIUS." ~ ".$gettemperaturenight.wechat::DEGREENS_CELSIUS." ".$getwinddir
+       ." ".$getwindpowlev
+       ."\n\n温馨提示："."天气".$getweatherkindlytipstatus."\n".$getweatherkindlytipmore
+       ."\n\n明天："
+       ."\n".$tom_weainfo." ".$tom_gettemperatureday.wechat::DEGREENS_CELSIUS." ~ ".$tom_gettemperaturenight.wechat::DEGREENS_CELSIUS." ".$tom_getwinddir
+       ." ".$tome_getwindpowlev
+       ."\n\n后天："
+       ."\n".$at_weainfo." ".$at_gettemperatureday.wechat::DEGREENS_CELSIUS." ~ ".$at_gettemperatureday.wechat::DEGREENS_CELSIUS." ".$at_getwinddir
+       ." ".$at_getwindpowlev;
      }else
      {
        $wearesult=wechat::WEATHERTIP;
